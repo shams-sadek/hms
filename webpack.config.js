fdaeb3973b2cd32a966b4b2713758ec1f92cefee
shams sadek <shams_sadek@yahoo.com>
2017-04-04 07:24:33 +0600

    2nd commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,76 +2,107 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+var webpack = require('webpack');
+
 const path = require('path');
 
-const webpack = require('webpack');
 
+/**
+ | -----------------------------------------------------------------------------
+ | css processing
+ | -----------------------------------------------------------------------------
+ */
+var isProd = process.env.NODE_ENV == 'production'; // true or false
 
-/*
- | -----------------------------------------------------------------------------
- | css mode
- | -----------------------------------------------------------------------------
- **/
-var isProd = process.env.NODE_ENV === 'production'; // true or false
 var cssDev = ['style-loader', 'css-loader', 'sass-loader'];
 
 var cssProd = ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader'],
-                    publicPath: '/dist'
-                });
+                  fallback: "style-loader",
+                  use: ['css-loader', 'sass-loader'],
+                  publicPath: "/dist"
+              });
 
 var cssConfig = isProd ? cssProd : cssDev;
 
 
+
+
+/**
+ | -----------------------------------------------------------------------------
+ | module exports
+ | -----------------------------------------------------------------------------
+ */
 module.exports = {
-    entry: path.resolve(__dirname, 'src') + '/app.js',
-    output: {
-        path: path.resolve(__dirname, "dist"), // string
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: cssConfig
-            },
-            {
-                test: /\.js$/,
-                // exclude: /node_modules/,
-                include: path.resolve(__dirname, "src"),
-                use: 'babel-loader',
-            },
-            {
-                test: /\.(jpe?g|gif|png|svg)$/i,
-                use: [
-                       'file-loader?name=[name].[ext]&outputPath=dist/images/&publicPath=src/images/',
-                       'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
-                ]
-            }
-        ]
-    },
-    devServer: {
-      contentBase: path.join(__dirname, "dist"),
-      compress: true,
-      hot: true,
-      port: 8080,
-      stats: 'errors-only'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-              title: 'My App',
-            //   minify: {
-            //       collapseWhitespace: true
-            //   },
-              hash: true,
-              filename: './../views/layouts/masterpage.hbs',
-              template: './src/index.ejs'
-        }),
-        new ExtractTextPlugin({
-            filename: "styles.css",
-            allChunks: true
-        }),
-        new webpack.HotModuleReplacementPlugin()
-    ]
+        entry: {
+            app: [
+                path.resolve(__dirname, "src/app.js"),
+                path.resolve(__dirname, "src/other.js")
+            ],
+            contact: path.resolve(__dirname, "src/contact.js"),
+        },
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: '[name].bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: cssConfig
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: "babel-loader"
+                },
+                {
+                    test: /\.(jpe?g|png|gif|svg)$/i,
+                    use: [
+                        'file-loader?name=images/[name].[ext]',
+                        'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                    ],
+
+                }
+            ]
+        },
+        devServer: {
+          contentBase: path.join(__dirname, "dist"),
+          compress: true,
+          port: 8081,
+          stats: 'errors-only',
+          hot: true
+        },
+        plugins: [
+                new HtmlWebpackPlugin({
+                  title: 'HMS',
+                //   minify: {
+                //       collapseWhitespace: true,
+                //   },
+                  excludeChunks: ['contact'],
+                  hash: true,
+                  filename: 'index.html',
+                  template: './src/index.html'
+              }),
+
+                new HtmlWebpackPlugin({
+                  title: 'Contact Page',
+                //   minify: {
+                //       collapseWhitespace: true,
+                //   },
+                  chunks: ['contact'],
+                  hash: true,
+                  filename: 'contact.html',
+                  template: './src/contact.html'
+              }),
+
+              new ExtractTextPlugin({
+                  filename: "styles.css",
+                  disable: !isProd,
+                  allChunks: true
+              }),
+
+              new webpack.HotModuleReplacementPlugin(),
+
+              new webpack.NamedModulesPlugin(),
+        ]// plugins
 }
